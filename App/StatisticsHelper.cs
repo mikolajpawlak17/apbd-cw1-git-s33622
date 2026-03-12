3970c24dcad02b1a94e8211bd342ba7b1513231b
Mikołaj Pawlak <s33622@pjwstk.edu.pl>
2026-03-12 19:41:57 +0100

    Implement CalculateAverage

--- App/StatisticsHelper.cs
+++ App/StatisticsHelper.cs
@@ -4,6 +4,9 @@ public class StatisticsHelper
 {
     public static double CalculateAverage(int[] numbers)
     {
-        return 0.0;
+        if (numbers == null || numbers.Length == 0) return 0.0;
+        double sum = 0;
+        foreach (var num in numbers) sum += num;
+        return sum / numbers.Length;
     }
 }
