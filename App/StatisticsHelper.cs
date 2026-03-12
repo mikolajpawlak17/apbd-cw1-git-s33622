b9840b21f9cf7ee2f07a07d248cdea44c6e46ff6
Mikołaj Pawlak <s33622@pjwstk.edu.pl>
2026-03-12 19:54:12 +0100

    Add method "GetMax" to class "StatisticsHelper"

--- App/StatisticsHelper.cs
+++ App/StatisticsHelper.cs
@@ -6,7 +6,19 @@ public class StatisticsHelper
     {
         if (numbers == null || numbers.Length == 0) return 0.0;
         double sum = 0;
-        foreach (var num in numbers) sum += num;
+        foreach (var num in numbers) 
+            sum += num;
         return sum / numbers.Length;
+    }
+
+    public static int GetMax(int[] numbers)
+    {
+        int max = numbers[0];
+        foreach (var num in numbers)
+        {
+            if(num > max)  
+                max = num;
+        }
+        return max;
     }
 }
