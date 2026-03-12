9d7f2619728402dc5aaa2a5ec4486def9221d40f
Mikołaj Pawlak <s33622@pjwstk.edu.pl>
2026-03-12 19:51:26 +0100

    CalculateAverage in Program.cs

--- App/Program.cs
+++ App/Program.cs
@@ -1,1 +1,5 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using App;
+
+int[] numbers = {1,2,3,4,5,6,7};
+double avg = StatisticsHelper.CalculateAverage(numbers);
+Console.WriteLine("Average: " + avg);
