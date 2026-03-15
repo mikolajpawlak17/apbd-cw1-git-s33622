edd7983ba9de66f3ac448dc00aaced54569b79af
Mikołaj Pawlak <s33622@pjwstk.edu.pl>
2026-03-15 13:10:26 +0100

    Change the name of variable in "CalculateMin"

--- App/StatisticsHelper.cs
+++ App/StatisticsHelper.cs
@@ -24,12 +24,12 @@ public class StatisticsHelper
 
     public static int CalculateMin(int[] numbers)
     {
-        int min = numbers[0];
+        int minValue = numbers[0];
         foreach (var num in numbers)
         {
-            if(num < min)  
-                min = num;
+            if(num < minValue)  
+                minValue = num;
         }
-        return min;
+        return minValue;
     }
 }
